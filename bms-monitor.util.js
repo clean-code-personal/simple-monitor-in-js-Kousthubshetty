c3fde21144a4c120af2c3fb2c1cfb9bbc24d6f78
Kousthubshetty <kousthubshetty@gmail.com>
2023-11-23 17:27:35 +0530

    Assignment completed

---
+++ bms-monitor.util.js
@@ -0,0 +1,36 @@
+const{logMessage} = require('./bms-monitor.logs');
+
+const factors = {
+    temperature:{ name:'Temperature', min:0, max:45 },
+    stateOfCharge:{ name:'State of Charge', min:20, max:80 },
+    chargeRate:{ name:'Charge rate', min:0, max:0.8 }
+};
+// function isDefined(value){
+//     if (value === undefined){
+//         // logMessage('error', batteryName, value, `${factors[factor].name} is unknown.`);
+//         console.log('error', batteryName, value, `${factors[factor].name} is unknown.`);
+//         return false;
+//     }else {
+//         return true;
+//     }
+// }
+function checkBattery(batteryName, factor, value){
+    let isOk=true
+    // if (value === undefined){
+    //     // logMessage('error', batteryName, value, `${factors[factor].name} is unknown.`);
+    //     console.log('error', batteryName, value, `${factors[factor].name} is unknown.`);
+    //     return false;
+    // }else 
+    if (value < factors[factor].min) {
+        logMessage('warning', batteryName, value, `${factors[factor].name} is out of range!`);
+        // console.log('warning', batteryName, value, `${factors[factor].name} is out of range!`);
+        isOk= false;
+    }else if (value > factors[factor].max) {
+        logMessage('warning', batteryName, value, `${factors[factor].name} is out of range!`);
+        // console.log('warning', batteryName, value, `${factors[factor].name} is out of range!`);
+        isOk= false;
+    }
+    return isOk;
+}
+
+module.exports={checkBattery};
